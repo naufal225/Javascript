53eece2a251f387a6db54ee17cf27f886d8195aa
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-05-31 21:20:51 +0700

    Create script.js

---
+++ Javascript Basic/1.1 Tipe Data/script.js
@@ -0,0 +1,14 @@
+// Tipe Data
+/*
+Ada tipe data number, boolean, string, object, function dan undefined
+*/
+
+let angka = 10 // Int
+let angka2 = 3.14 // Float
+let nama = "Naufal" // String
+let yes = true // Boolean
+
+console.log(angka) // menampilkan nilai kw layar
+console.log(angka2)
+console.log(nama)
+console.log(yes)
