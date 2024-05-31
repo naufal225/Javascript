e225ad7b99cdc4ea022e6cc05528ff8e1c1765c3
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-01 06:57:49 +0700

    Add files via upload

---
+++ Javascript Basic/2.1 Number/script.js
@@ -0,0 +1,15 @@
+let angka1 = 0.2
+let angka2 = 0.1
+
+let angka3 = 10
+let angka4 = 120
+
+let angka5 = 12e2 // 1200
+let angka6 = angka1 + angka2
+
+console.log(angka1)
+console.log(angka2)
+console.log(angka6)
+console.log(angka3)
+console.log(angka4)
+console.log(angka5)
