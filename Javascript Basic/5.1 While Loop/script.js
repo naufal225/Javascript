cad7743edbbea8c691dc0ff04b3a7add10c470d1
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-02 06:07:52 +0700

    Create script.js

---
+++ Javascript Basic/5.1 While Loop/script.js
@@ -0,0 +1,13 @@
+let ulang = true;
+let i = 1;
+let x = 1;
+while(ulang){
+  document.write("Perulangan ke-" + i + "<br>");
+  ulang = confirm("Lagi?");
+  i++;
+}
+
+while(x<=10) {
+  console.log("Angkot-" + x + " berjalan dengan baik");
+  x++;
+}
