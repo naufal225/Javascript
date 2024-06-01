d5fe6905bb76ce1cb7bedff9d079ab010e62872d
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-01 07:48:42 +0700

    Create script.js

---
+++ Javascript Basic/2.3 String/script.js
@@ -0,0 +1,23 @@
+// String
+
+let nama = "Naufal Ma'ruf Ashrori"
+
+console.log(nama)
+
+// Konkatenasi
+let text = "Nama saya adalah " + nama
+console.log(text)
+
+// Membandingkan String
+
+console.log("naufal"=="Naufal") //false
+
+// mengakses panjang String
+
+console.log(nama.length)
+console.log(text.length)
+
+// Mengakses karakter pada string dwngan index
+
+console.log(nama[0])
+console.log(text[9])
