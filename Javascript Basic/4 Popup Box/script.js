bacc8d78448701044d4584b12fd9fd90040211dc
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-01 18:00:47 +0700

    Create script.js

---
+++ Javascript Basic/4 Popup Box/script.js
@@ -0,0 +1,15 @@
+// POPUP BOX
+
+// alert
+
+alert("Selamat Datang");
+
+// prompt
+
+let nama = prompt("Masukan nama");
+document.write("<h1>" + nama + "</h1>");
+
+// confirm
+
+let tes = confirm("yes?");
+alert(tes)
