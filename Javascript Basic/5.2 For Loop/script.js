fdffb5f8d4cd215f38c3c12ef93796ce62a696c6
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-02 06:23:53 +0700

    Create script.js

---
+++ Javascript Basic/5.2 For Loop/script.js
@@ -0,0 +1,9 @@
+let noAngkot = 1, jmlAngkot = 10, angkotBeroperasi = 6;
+while(noAngkot<=angkotBeroperasi) {
+  console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
+  noAngkot++;
+}
+
+for(noAngkot; noAngkot<=jmlAngkot; noAngkot++) {
+  console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi")
+}
