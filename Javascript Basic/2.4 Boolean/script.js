1c734a24777e4d2b86952dcbcf34458bee876cbd
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-01 17:02:28 +0700

    Create script.js

---
+++ Javascript Basic/2.4 Boolean/script.js
@@ -0,0 +1,13 @@
+// Boolean
+// Tipe data boolean adalah tipe data yang digunakan
+// untuk merepresentasikan logika true atau false
+
+var a = 9 // variabel isi
+let b // variabel kosong
+
+console.log(a<10) //true
+console.log(Boolean("")) //false
+console.log(Boolean("haha")) //true
+console.log(Boolean(100)) //true
+console.log(Boolean(0)) //false
+console.log(Boolean(b)) //false
