4a7b7e738f6db5597bbe9740e25ad9198ea3e2e0
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-01 17:20:09 +0700

    Create script.js

---
+++ Javascript Basic/3 Variable/script.js
@@ -0,0 +1,11 @@
+// Variable
+// alat yang digunakan untuk menyimpan nilai
+
+var x; // deklarasi dan inisialisasi
+x = 10; // assignment
+
+var nama = "Naufal", umur = 16, siswa = true
+
+console.log(nama)
+console.log(umur)
+console.log(siswa)
