78a5f472917aa03eed404f654f82c0b0e87ff42c
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-02 06:32:14 +0700

    Create script.js

---
+++ Javascript Basic/5.3 If Else/script.js
@@ -0,0 +1,8 @@
+let noAngkot = 1, jmlAngkot = 10, angkotBeroperasi = 6;
+for(noAngkot; noAngkot<=jmlAngkot; noAngkot++) {
+  if(noAngkot<=angkotBeroperasi) {
+    console.log("Angkot No. " + noAngkot + " beroperasi dengan baik");
+  } else {
+    console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi");
+  }
+}
