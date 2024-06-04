94cf7c654f280cd745bf7a5e43994a3846f4421c
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-04 19:00:07 +0700

    Update script.js

--- Javascript Basic/8.0 Object/script.js
+++ Javascript Basic/8.0 Object/script.js
@@ -1,7 +1,7 @@
 // Object
 // secara garis besar, object adalah kumpulan nilai yang mempunyai nama
 
-// membuat object
+// membuat object secara literal
 
 var siswa1 = {
     nama: "Naufal Ma'ruf Ashrori",
@@ -16,3 +16,43 @@ var siswa1 = {
         return sum/total
     }
 }
+
+// membuat object dengan function declaration
+
+function buatSiswa(nama,nis,nilai) {
+    var siswa = {
+        nama: nama,
+        nis: nis,
+        nilai: nilai,
+        rata: function() {
+            var sum = 0, total = 0;
+            this.nilai.forEach(function(e){
+                sum += e;
+                total++;
+            });
+            return sum/total
+        }
+    }
+    return siswa;
+}
+
+var siswa2 = buatSiswa("Budi", "232410102", [80,90,100,60,70]);
+
+
+// membuat object menggunakan constructor
+
+function Siswa(nama,nis,nilai) {
+    this.nama = nama;
+    this.nis = nis;
+    this.nilai = nilai;
+    this.rata = function() {
+        var sum = 0, total = 0;
+            this.nilai.forEach(function(e){
+                sum += e;
+                total++;
+            });
+            return sum/total
+    }
+}
+
+var siswa3 = new Siswa("Eko", "232410100", [80,90,100,50,90]); //harus dengan keyword new
