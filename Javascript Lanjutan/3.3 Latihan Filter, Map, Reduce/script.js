198c85f2acb661848b3ca0c06b67df5bfc095789
Naufal Ma'ruf Ashrori <128896038+naufal225@users.noreply.github.com>
2024-06-07 19:52:23 +0700

    Update script.js

--- Javascript Lanjutan/3.3 Latihan Filter, Map, Reduce/script.js
+++ Javascript Lanjutan/3.3 Latihan Filter, Map, Reduce/script.js
@@ -1,30 +1,29 @@
-// ambil semua elemen video
-const videos = Array.from(document.querySelectorAll('[data-duration]')); // ambil element yang ada atribut data-duration
+// Ambil semua elemen video
+const videos = Array.from(document.querySelectorAll('[data-duration]'));
 
-// pilih hanya playlist lanjutan
-const playlist_lanjutan = videos.filter(video => video.textContent.includes("PLAYLIST LANJUTAN |"))
+// Pilih hanya playlist lanjutan
+const playlist_lanjutan = videos.filter(video => video.textContent.includes('PLAYLIST LANJUTAN |'))
 
 // ambil durasi masing masing video
-    .map(video => video.dataset.duration)
+.map(video => video.dataset.duration)
 
-// ubah durasi menjadi int, ubah menit jadi detik
-    .map(waktu => {
-        const parts = waktu.split(":");
-        return (parseInt(parts[0])*60) + parseInt(parts[1]);
-    })
+// ubah menjadi int detik
+.map(waktu => {
+    const [menit, detik] = waktu.split(':');
+    return (parseInt(menit) * 60) + parseInt(detik);
+})
 
-// jumlahkan semua detik
-    .reduce((acc, v) => acc + v);
+// jumlah semua detik
+.reduce((acc,v) => acc + v);
 
-// ubah jadi format jam menit detik
+// ubah jadi format jam, menit, detik
 const jam = Math.floor(playlist_lanjutan / 3600);
-const menit = Math.floor((playlist_lanjutan / 60) % 60);
+const menit = Math.floor(playlist_lanjutan % 3600 /60);
 const detik = Math.floor(playlist_lanjutan % 60);
 
-// simpan di DOM
+// Masukan ke DOM
 
 const pDurasi = document.querySelector('.jumlah-durasi');
 pDurasi.textContent = `${jam} jam, ${menit} menit, ${detik} detik`;
-
 const jVideo = document.querySelector('.jumlah-video');
-jVideo.textContent = videos.length;
+jVideo.textContent = `${videos.length} Video`;
